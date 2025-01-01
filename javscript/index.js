df0fad85390098ea14bc62b5f0abf5671cb1607a
Irshad <irshadutkoori@gmail.com>
2025-01-01 15:44:00 +0530

    main section of index html designed

--- javscript/index.js
+++ javscript/index.js
@@ -1,8 +1,17 @@
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector("nav")
+const navLinks = document.querySelector("nav");
+const mainSection = document.getElementById("main-section");
 
-hamburger.addEventListener("click", function(){
-  navLinks.classList.toggle("active")
+//toggle button setting
+hamburger.addEventListener("click", function () {
+  navLinks.classList.toggle("active");
   // navLinks.style.backgroundColor = "red"
-  
+  if (navLinks.classList.contains("active")) {
+    mainSection.classList.add('shifted'); // Adjust this value based on navbar height
+  } else {
+    mainSection.classList.remove('shifted') // Reset the margin when navbar is hidden
+  }
 });
+
+//
+
