5670bb74b76d360175bdd61e3c3323ab41855913
Irshad <irshadutkoori@gmail.com>
2025-01-01 14:25:03 +0530

    index file designing

--- javscript/index.js
+++ javscript/index.js
@@ -1,5 +1,5 @@
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById('nav-links')
+const navLinks = document.querySelector("nav")
 
 hamburger.addEventListener("click", function(){
   navLinks.classList.toggle("active")
